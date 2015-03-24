dbabf49862b6a42fcc39d988488bc00b65069e98
KirbiKaka <calvinlu@berkeley.edu>
2015-03-23 20:23:01 -0700

    Opening Dialogue stuff

---
+++ FantacticalFrontier/Assets/Scripts/Dialogue.cs
@@ -0,0 +1,68 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Dialogue : MonoBehaviour {
+
+    public Texture dialogueBox;
+    public Texture sentinel;
+    public Texture bartender;
+    public Texture astronomer;
+    public Texture patrol;
+    public Texture sharpshooter;
+    public Texture zealot;
+
+    private bool boxVisible = false;
+    private string message = "";
+
+    // Character images during dialogue.
+    private string[] leftImages;
+    private string[] rightImages;
+
+	// Use this for initialization
+	void Start () {
+        transform.position = new Vector3(0, Screen.height * 2 / 3, 0);
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (Input.GetButtonUp("Fire1")) {
+            if (boxVisible) {
+                SetBoxVisible(false);
+            }
+        }
+	}
+
+    void OnGUI () {
+        if (boxVisible) {
+            if (leftImages != null) {
+                // TODO iterate through and draw each image with a slight offset, use GetTextureFromString
+            }
+            GUI.DrawTexture(new Rect(0, Screen.height * 2 / 3, Screen.width, Screen.height / 3), dialogueBox, ScaleMode.ScaleToFit);
+            GUI.Label(new Rect(0, Screen.height * 2 / 3, Screen.width, Screen.height / 3), message);
+        }
+    }
+
+    void OnMouseClick () {
+    }
+
+    public void ShowText (string m, string[] left = null, string[] right = null) {
+        SetBoxVisible(true);
+        message = m;
+        leftImages = left;
+        rightImages = right;
+    }
+
+    private Texture GetTextureFromString (string s) {
+        // TODO
+        return null;
+    }
+
+    public bool GetBoxVisible () {
+        return boxVisible;
+    }
+    public void SetBoxVisible (bool display) {
+        boxVisible = display;
+    }
+
+}
---
+++ FantacticalFrontier/Assets/Scripts/DialogueOpening.cs
@@ -0,0 +1,50 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class DialogueOpening : MonoBehaviour {
+
+    public GameObject dialogueHandler;
+
+    // The current step of dialogue. One array of messages.
+    private int step = 0;
+    // The current step in the array of messages.
+    private int arrayStep = 0;
+    // Finished with the current step. Stop sending messages until the next step.
+    private bool finished = false;
+    private string[] messages;
+
+	// Use this for initialization
+	void Start () {
+        LoadMessages(step);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (messages != null && arrayStep < messages.Length) {
+            if (!dialogueHandler.GetComponent<Dialogue>().GetBoxVisible()) {
+                dialogueHandler.GetComponent<Dialogue>().ShowText(messages[arrayStep]);
+                arrayStep++;
+            }
+        }
+	}
+
+    public void NextStep (int s = 10) {
+        step += s;
+        finished = false;
+        LoadMessages(step);
+    }
+
+    private void LoadMessages (int s) {
+        switch (s) {
+            case 0:
+                messages = new string[2];
+                messages[0] = "TESTESTESTESTETSTEESD";
+                messages[1] = "EVENMORE";
+                break;
+        }
+    }
+
+    public bool IsFinished () {
+        return finished;
+    }
+}
